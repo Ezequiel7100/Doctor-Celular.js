0c35b03c0d367098bcfe727f61f2c5430df6605f
Ezequiel Hermoso <hermosoezequiel352@gmail.com>
2022-09-29 18:31:04 -0300

    segunda pre-entrega

--- app.js
+++ app.js
@@ -1,126 +1,150 @@
-
-
-let usuario = prompt("ingrese su usuario");
-
-alert("Hola!! " + usuario + " Bienvenido/a a Doctor Celular")
-
-
-const productos = [
-    {nombre: "G60s" , precio: 60000 },
-    {nombre: "A12" , precio: 45000 },
-    {nombre: "A22" , precio: 68000 },
-]
+const clickboton = document.querySelectorAll('.boton')
+const tbody = document.querySelector('.tbody')
 let carrito = []
 
-let seleccion = prompt(`Desea comprar algun producto?
-si (Para comprar)
-no (Para salir)
-coloque si o no para continuar`)
 
-compararSiNo()
+clickboton.forEach(btn => {
+    btn.addEventListener('click', guardarItemCarrito)
+});
 
-function compararSiNo() {
-    
-    while (seleccion != "si" && seleccion != "no") {
-        alert("Por favor ingrese si o no")
-        seleccion = prompt(`Desea comprar algun producto?
-    si (Para comprar)
-    no (Para salir)
-    coloque si o no para continuar`)
+function guardarItemCarrito(e){
+    const button = e.target
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent;
+    const itemPrecio = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.img').src;
+
+    const nuevoItem = {
+        titulo: itemTitle,
+        precio: itemPrecio,
+        img: itemImg,
+        cantidad: 1
     }
-}  
 
-siNoUsuario()
+    guardarEnCarrito(nuevoItem)
+}
+    
 
-function siNoUsuario() {
-    if (seleccion == "si") {
-        alert("Perfecto, abajo puede ver nuestros productos")
-    } else if (seleccion == "no"){
-        alert("Gracias por ingresar a nuestra pagina")
+function guardarEnCarrito(nuevoItem) {
+
+    const alert = document.querySelector('.alert')
+
+    setTimeout( function(){
+      alert.classList.add('alerta')
+    }, 1500)
+      alert.classList.remove('alerta')
+
+for (let i = 0; i < carrito.length; i++) {
+
+const inputElemento = tbody.getElementsByClassName('input__elemento')
+
+
+    if (carrito[i].titulo.trim() === nuevoItem.titulo.trim()) {
+        carrito[i].cantidad ++;
+        const inputValue = inputElemento[i]
+        inputValue.value++
+        CarritoTotal()
+        return null;
+    }
+    
+}
+
+
+    carrito.push(nuevoItem)
+    renderCarrito()
+
+}
+
+function renderCarrito() {
+    tbody.innerHTML = ''
+    carrito.map(item => {
+        const tr = document.createElement('tr')
+        tr.classList.add('itemCarrito' )
+        const contenido = `
         
-    }
-}
-
-let producto;
-
-function mostrarOferta() {
-    const ofertas = productos.filter((el) => el.precio < 50000);
-    for(const oferta of ofertas){
-    alert("La oferta de hoy es: " + oferta.nombre + " a un valor de $" + oferta.precio);
-    }
-} 
-
-while (seleccion != "no") {
-    producto = prompt(`Ingrese el nombre del celular que desea comprar para añadir al carrito:
-     ${productos.map((productos, index) => {
-        return `\nn° ${index + 1}: ${productos.nombre} con un precio de $${productos.precio}`
-     })}
-     \no ingrese of para ver la oferta de hoy`)
-    
-    if (producto == "A12" || producto == "A22" ||producto == "G60s"  ) {
+        <th scope="row">1</th>
+                    <td class="tabla__productos">
+                        <img src= ${item.img} alt="">
+                        <h6 class="title">${item.titulo}</h6>
+                    </td>
+                    <td class="tabla__precio">
+                        <p>${item.precio}</p>
+                    </td>
+                    <td class="tabla__cantidad">
+                        <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+                        <button class="delete btn btn-danger">x</button>
+                    </td>
         
-        let precio = prompt("ingrese el precio")
-        let unidades = parseInt(prompt("¿Cuantas unidades quiere comprar?"))
+        `
+        tr.innerHTML = contenido;
+        tbody.append(tr)
 
-        carrito.push({producto, unidades , precio})
-        console.log(carrito)
-
-    }
-    else if (producto === "of") {
-        mostrarOferta(producto)
-    }
-    else{
-        alert("No ingreso un producto valido")
-    }
-
-    seleccion = prompt(`Desea seguir comprando?
-    si (Para seguir comprando)
-    no (Para salir y finalizar compra)
-    coloque si o no para continuar`)
-
-
-    while(seleccion == "no"){
-        alert("Gracias por su compra! Hasta pronto")
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},
-            total a pagar por producto: $${carritoFinal.unidades * carritoFinal.precio} `)
-        })
-        break;
-    }
-
-}
-const total = carrito.reduce((acc , el) => acc + el.precio * el.unidades, 0)
-
-    alert(`El total a pagar por su compra: ${total}`)
-    
-    alert("Muchas Gracias por visitarnos!")
-
-let eventoBoton1 = document.getElementById("boton1");
-let eventoBoton2 = document.getElementById("boton2");
-let eventoBoton3 = document.getElementById("boton3");
-
-eventoBoton1.addEventListener("click", evento )
-eventoBoton2.addEventListener("click", evento )
-eventoBoton3.addEventListener("click", evento )
-
-function evento(){
-alert("Producto Agregado correctamente al carrito")
+        tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+        tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
 }
 
-function mensajeEnviar(){
-alert("Email ingresado correctamente")
+
+function CarritoTotal() {
+    let total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) => {
+        const precio = Number(item.precio.replace("$", '' ))
+        total = total + precio*item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total: $${total}`
+    guardarLocalStorage()
 }
 
-const formulario = document.getElementById("from")
 
-const mail = document.getElementById("email")
+function removeItemCarrito(e){
+    const botonDelete = e.target
+    const tr = botonDelete.closest(".itemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for (let i = 0; i < carrito.length; i++) {
+        
+        if(carrito[i].titulo.trim() === title.trim()){
+            carrito.splice(i, 1)
+        }
+    }
 
-formulario.addEventListener("submit", validarFomulario) 
+    const alert = document.querySelector('.eliminar')
 
-function validarFomulario() {
-    console.log(mail.value);
+    setTimeout( function(){
+      alert.classList.add('eliminar')
+    }, 1500)
+      alert.classList.remove('eliminar')
+
+    tr.remove()
+    CarritoTotal()
 }
 
-let btnEnviar = document.getElementById("enviar")
- 
-btnEnviar.addEventListener("click", mensajeEnviar )
+
+function sumaCantidad(e){
+    const sumaInput = e.target
+    const tr = sumaInput.closest(".itemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item => {
+        if (item.titulo.trim() === title) {
+            sumaInput.value < 1 ? (sumaInput.value = 1) : sumaInput.value;
+            item.cantidad = sumaInput.value;
+            CarritoTotal()
+        }
+     })
+}
+
+function guardarLocalStorage(){
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+
+
+window.onload = function (){
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if (storage) {
+        carrito = storage;
+        renderCarrito()
+    }
+}
